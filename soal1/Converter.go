99d3cf6d77a8860a5a08f840467114a8aaf480f3
Zaki_Khairi <zakielbavaian@gmail.com>
2020-04-27 14:07:21 +0700

    init project go

---
+++ soal1/Converter.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	rupiah := 15.52950
+	dollar := 4.0
+	jumlah := dollar * rupiah
+
+	fmt.Printf("Jumlah Rupiah dalam %f USD adalah: %f IDR", dollar, jumlah)
+	
+}
---
+++ soal2/HitungLuas.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	diameter := 17.0
+	pi := 3.14
+	jumlah := 0.25 * pi * diameter
+
+	fmt.Printf("Luas Lingkaran dengan diameter %f adalah: %f", diameter, jumlah)
+
+}
